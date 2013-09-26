f343fbc25fd160809a8858a19701db8cb71767ac
Jan van der Pas <janvanderpas@gmail.com>
2013-09-26 02:07:44 +0200

    Refactor luhn method, exclude of include check_digit for Luhn algoritme

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -13,20 +13,26 @@ module Luhn
     end
 
     def calculate_check_digit(number)
-      doubles = double_digits(number)
-      sum_of_all(doubles) unless null?(doubles)
+      number_without_check_digit = number.to_s[0...-1] + '0'
+      doubles = double_digits(number_without_check_digit.to_i)
+      sum = sum_of_all(doubles) unless null?(doubles)
+      product = sum * 9
+      get_check_digit(product)
+    end
+
+    def get_check_digit(number)
+      number.to_s[-1].to_i
     end
 
     def double_digits(number)
       doubles = []
       digits = all_digits(number).reverse
       digits.each_with_index do |d, i|
-        if i.odd? == true
+        if i.odd?
           sum = d * 2
-          if sum > 9 || !sum.nil?
-            split_sum   = sum.to_s.split(//)
-            to_int      = split_sum.map { |m| m.to_i }
-            make_sum    = to_int.inject { |sum, n| sum + n }
+          if sum && sum > 9
+            split_sum   = sum.to_s.split(//).map(&:to_i)
+            make_sum    = split_sum.inject { |sum, n| sum + n }
             doubles     << make_sum
           else
             doubles << sum
@@ -41,11 +47,12 @@ module Luhn
     protected
 
     def checksum(number)
-      check_digit = calculate_check_digit(number)
-      unless check_digit.nil?
-        check_digit % 10 == 0 ? true : false
-      else
+      if null?(number)
         return false
+      else
+        doubles = double_digits(number)
+        sum = sum_of_all(doubles)
+        sum % 10 == 0 ? true : false
       end
     end
 
@@ -54,13 +61,16 @@ module Luhn
     end
 
     def all_digits(number)
-      ary = number.to_s.split(//)
-      return ary.map { |string| string.to_i }
+      number.to_s.split(//).map(&:to_i)
     end
 
-    def null?(doubles)
-      checked_doubles = doubles.reject { |d| d.eql?(0) }
-      return true if checked_doubles.empty?
+    def null?(number)
+      checked_number = all_digits(number).reject! { |d| d.eql?(0) }
+      if checked_number.nil?
+        return false
+      else
+        checked_number.empty? ? true : false
+      end
     end
 
   end
--- test/luhn_test.rb
+++ test/luhn_test.rb
@@ -8,17 +8,35 @@ describe 'Luhn' do
 
   describe 'check_digit' do
 
-    describe '.calculate_check_digit' do
+    describe 'only for valid numbers' do
 
-      it 'computes the check digit of the .sum_of_all' do
-        Luhn.calculate_check_digit(123).must_equal(8)
-        Luhn.calculate_check_digit(492965255140195).must_equal(60)
-        Luhn.calculate_check_digit(402400711116634).must_equal(46)
-        Luhn.calculate_check_digit(199600).must_equal(26)
-        Luhn.calculate_check_digit(700).must_equal(7)
+      describe '.calculate_check_digit' do
+
+        it 'computes the check digit' do
+          Luhn.calculate_check_digit(79927398713).must_equal(3)
+          Luhn.calculate_check_digit(4539085529167499).must_equal(9)
+          Luhn.calculate_check_digit(6011312159763625).must_equal(5)
+          Luhn.calculate_check_digit(4111111111111111).must_equal(1)
+          Luhn.calculate_check_digit(180002230256255).must_equal(5)
+          Luhn.calculate_check_digit(6011312159763625).must_equal(5)
+        end
       end
     end
 
+    describe 'for all numbers' do
+
+      describe '.get_check_digit' do
+
+        it 'get last digit of number' do
+          Luhn.get_check_digit(123).must_equal(3)
+          Luhn.get_check_digit(7992739871).must_equal(1)
+          Luhn.get_check_digit(492965255140195).must_equal(5)
+          Luhn.get_check_digit(402400711116634).must_equal(4)
+          Luhn.get_check_digit(199600).must_equal(0)
+          Luhn.get_check_digit(4556974027974373).must_equal(3)
+        end
+      end
+    end
   end
 
   describe '.double_digits' do
@@ -28,10 +46,11 @@ describe 'Luhn' do
       it 'computes the sum the digits of the sum' do
         Luhn.double_digits(50).must_equal([0,1])
         Luhn.double_digits(60).must_equal([0,3])
+        Luhn.double_digits(9).must_equal([9])
         Luhn.double_digits(90).must_equal([0,9])
         Luhn.double_digits(123).must_equal([3,4,1])
-        Luhn.double_digits(4992739871).must_equal([1,5,8,9,3,5,2,9,9,8])
-        Luhn.double_digits(7992739871).must_equal([1,5,8,9,3,5,2,9,9,5])
+        Luhn.double_digits(4992739871).must_equal([1, 5, 8, 9, 3, 5, 2, 9, 9, 8])
+        Luhn.double_digits(7992739871).must_equal([1, 5, 8, 9, 3, 5, 2, 9, 9, 5])
       end
     end
 
@@ -40,7 +59,7 @@ describe 'Luhn' do
       it 'computes the double of the digit' do
         Luhn.double_digits(10).must_equal([0,2])
         Luhn.double_digits(40).must_equal([0,8])
-        Luhn.double_digits(50).wont_equal([10])
+        Luhn.double_digits(50531).must_equal([1,6,5,0,5])
       end
     end
 
