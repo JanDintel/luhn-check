0f531f1c2fc31041da9ea70d68804db1aaa7025d
Jan van der Pas <janvanderpas@gmail.com>
2013-11-25 22:24:04 +0100

    DRY up the Base class

--- lib/luhn/base.rb
+++ lib/luhn/base.rb
@@ -15,19 +15,16 @@ class Base
   end
 
   def sum_of_all_numbers
-    double_digit_on_even_position.inject { |sum, n| sum + n }
+    double_digit_on_even_position.inject(:+)
   end
 
   def double_digit_on_even_position
-    # Reverse number_to_validate to start
-    # the itteration from right to left
     total = []
-    digits_of_number_to_validate = split_to_number_array(number_to_validate).reverse
-    # Offset of 1, so you can call even? instead of odd? on number_position
+    # Offset of 1, so you can call even? instead of odd? on a number_position
     digits_of_number_to_validate.to_enum.with_index(1).each do |digit, number_position|
       if number_position.even? && product_exceeds_nine(digit)
-        digits_of_product = split_to_number_array(multiply(digit))
-        total << digits_of_product.inject { |sum, n| sum + n }
+        digits_of_product = split_to_number_array multiply(digit)
+        total << digits_of_product.inject(:+)
       elsif number_position.even? && !product_exceeds_nine(digit)
         total << multiply(digit)
       else
@@ -38,6 +35,12 @@ class Base
   end
 
   private
+
+  def digits_of_number_to_validate
+    # Reverse number_to_validate to start
+    # the itteration from right to left
+    split_to_number_array(number_to_validate).reverse
+  end
 
   def split_to_number_array(numbers)
     numbers.to_s.split(//).map(&:to_i)
