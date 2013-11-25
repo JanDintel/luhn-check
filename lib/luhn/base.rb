7082afc6daf87d506a0b073861d5245f5ff9d0f8
Jan van der Pas <janvanderpas@gmail.com>
2013-11-25 22:11:20 +0100

    Make the internal methods and variables more sementic

--- lib/luhn/base.rb
+++ lib/luhn/base.rb
@@ -21,14 +21,14 @@ class Base
   def double_odd_numbers
     # Reverse number_to_validate to start
     # the itteration from right to left
-    digits_of_number = split_to_numbers(number_to_validate).reverse
     total = []
-    digits_of_number.each_with_index do |digit, i|
-      if i.odd? && product_exceeds_nine(digit)
-        split_product = split_to_numbers(product(digit))
-        total << split_product.inject { |sum, n| sum + n }
-      elsif i.odd? && !product_exceeds_nine(digit)
-        total << product(digit)
+    digits_of_number_to_validate = split_to_number_array(number_to_validate).reverse
+    digits_of_number_to_validate.each_with_index do |digit, number_position|
+      if number_position.odd? && product_exceeds_nine(digit)
+        digits_of_product = split_to_number_array(multiply(digit))
+        total << digits_of_product.inject { |sum, n| sum + n }
+      elsif number_position.odd? && !product_exceeds_nine(digit)
+        total << multiply(digit)
       else
         total << digit
       end
@@ -38,15 +38,15 @@ class Base
 
   private
 
-  def split_to_numbers(numbers)
+  def split_to_number_array(numbers)
     numbers.to_s.split(//).map(&:to_i)
   end
 
   def product_exceeds_nine(digit)
-    product(digit) > 9
+    multiply(digit) > 9
   end
 
-  def product(digit)
+  def multiply(digit)
     digit * 2
   end
 end
