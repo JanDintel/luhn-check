55eef599c4d5e59ee0998912974bb6da706e28e9
Jan van der Pas <janvanderpas@gmail.com>
2013-12-24 23:41:54 +0100

    Delegate the .valid? method to the Base class

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -2,4 +2,8 @@ require 'luhn/version'
 require 'luhn/base'
 
 module Luhn
+
+  def self.valid?(number)
+    Luhn::Base.valid?(number)
+  end
 end
--- spec/lib/luhn_spec.rb
+++ spec/lib/luhn_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Luhn do
+
+  describe '.valid?' do
+    it 'delegates the method to the Base class' do
+      expect(Luhn::Base).to receive(:valid?).with(12345)
+      described_class.valid?(12345)
+    end
+  end
 end
